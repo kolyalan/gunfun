d3f0517700bae244fec513771259592d90f28d8a
Nikolay Lanbin <kolyalan@yandex.ru>
2018-05-14 19:07:10 +0300

    fixed images, rounded player

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var game = new Phaser.Game(1000, 600, Phaser.AUTO, "game-field", {preload:preloa
 
 function preload ()
 {
-	game.load.spritesheet('image', 'chelik.png', 100, 174);
+	game.load.spritesheet('image', 'chelik.png', 174, 100);
 	game.load.image('box', 'box0.png');
 	game.load.image('bullet', 'sprites/bullet.png');
 }
@@ -19,7 +19,7 @@ function create() {
 	game.time.advancedTiming = true;
 	game.input.mouse.capture = true;
 	game.physics.startSystem(Phaser.Physics.P2JS);
-	game.physics.p2.defaultRestitution = 1;
+	game.physics.p2.restitution = 0;
 	game.stage.backgroundColor = "#EEEEEE";
 
 
@@ -27,6 +27,10 @@ function create() {
 	game.physics.p2.enable(player1, true);
 	player1.frame = 1;
 	player1.animations.add('go', [1, 0, 1, 2], 7, true);
+	player1.enableBody = true;
+	player1.body.collideWorldBounds = true;
+	player1.anchor.setTo(0.45, 0.54);
+	player1.body.setCircle(65);
 
 
 	// https://phaser.io/examples/v2/weapon/bullet-angle-variance
@@ -35,19 +39,18 @@ function create() {
     //  The bullet will be automatically killed when it leaves the world bounds
     weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
     //  Because our bullet is drawn facing up, we need to offset its rotation:
-    weapon.bulletAngleOffset = -90;
+    //weapon.bulletAngleOffset = -90;
     //  The speed at which the bullet is fired
     weapon.bulletSpeed = 800;
     //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
     weapon.fireRate = 60;
     //  Add a variance to the bullet angle by +- this value
-    weapon.bulletAngleVariance = 10;
-	weapon.trackSprite(player1, 0, 0);
+    weapon.bulletAngleVariance = 5;
+	weapon.trackSprite(player1, 103, 0, true);
+	//weapon.bulletCollideWorldBounds = true;
+	//weapon.bulletEnableBody = true;
 
 
-	player1.enableBody = true;
-	player1.body.collideWorldBounds = true;
-	player1.anchor.setTo(0.5, 0.5);
 	cursors = game.input.keyboard.createCursorKeys();
 	game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
 	game.input.keyboard.addKeyCapture([ Phaser.Keyboard.A ]);
@@ -58,18 +61,15 @@ function create() {
 	boxes = game.add.physicsGroup(Phaser.Physics.P2JS);
 	var b0 = boxes.create(400, 200, 'box');//случайные координаты где-то в середине.
 	b0.anchor.setTo(0.5, 0.5);
-	//b0.body.static = true;
+	b0.body.kinematic = true;
 	b0.body.rotation = 0.8243;//случайное, вообще-то число
 	var b1 = boxes.create(500, 400, 'box');//случайные координаты где-то в середине.
 	b1.anchor.setTo(0.5, 0.5);
-	//b1.body.static = true;
+	b1.body.kinematic = true;
 	b1.body.rotation = 1.8243;//случайное, вообще-то число
-	b1.body.immovable = true;
 }
 
 function update() {
-	var hitBox = game.physics.arcade.collide(player1, boxes);
-
 	player1.body.setZeroVelocity();
 	if (cursors.left.isDown || game.input.keyboard.isDown(Phaser.Keyboard.A))
     {
@@ -95,15 +95,18 @@ function update() {
 		player1.animations.stop();
         player1.frame = 1;
 	}
-    player1.body.rotation = game.physics.arcade.angleToPointer(player1)+PI/2;
+    player1.body.rotation = game.physics.arcade.angleToPointer(player1);
 	weapon.fireAngle = Phaser.Math.radToDeg(game.physics.arcade.angleToPointer(player1));
     if(game.input.activePointer.leftButton.isDown) {
         weapon.fire();
     }
+    weapon.bullets.forEachAlive(function(bull){
+		//game.physics.arcade.collide(b0, bull); не работает
+		//console.log(bull.body.type);
+	});
 }
 
 function render() {
 	game.debug.text(game.time.fps, 2, 14, "#00ff00");
 	game.debug.body(player1);
-	game.debug.body(boxes);
 }
