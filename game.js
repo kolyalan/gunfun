4c8bfe6f75c8f40c7c14299e9e5fb52ca9d0d556
Nikolay Lanbin <kolyalan@yandex.ru>
2018-05-24 01:40:10 +0300

    tabs->spaces

--- game.js
+++ game.js
@@ -25,49 +25,49 @@ var game = new Phaser.Game(1000, 600, Phaser.AUTO, "game-field", {preload:preloa
 
 function Weapon(player_sprite, num_bullets, bullet_speed, fire_rate, max_coll, reloading_time) {
     this.player_sprite = player_sprite;
-	this.bullets = [];
-	this.bullet_ind = 0;
-	this.num_bullets = num_bullets;
-	this.bullet_speed = bullet_speed;
-	this.fire_rate = fire_rate;
-	this.prev_fire = -1000;
-	this.bullets_in_row = 0;
-	this.max_coll = max_coll;
+    this.bullets = [];
+    this.bullet_ind = 0;
+    this.num_bullets = num_bullets;
+    this.bullet_speed = bullet_speed;
+    this.fire_rate = fire_rate;
+    this.prev_fire = -1000;
+    this.bullets_in_row = 0;
+    this.max_coll = max_coll;
     this.needs_reload = false;
     this.reloading_time = reloading_time;
     this.reloading = false;
-	for(let i = 0; i < num_bullets; i++) {
-		let bullet = game.add.sprite(0, 0, 'bullet');
-		game.physics.p2.enable(bullet, false);
-		bullet.body.dynamic = true;
-		bullet.anchor.setTo(0.5, 0.5);
-		bullet.body.damping = 0; //part of velocity losed per second [0..1];
-		bullet.body.setCircle(7);
-		bullet.body.fixedRotation = false;
+    for(let i = 0; i < num_bullets; i++) {
+        let bullet = game.add.sprite(0, 0, 'bullet');
+        game.physics.p2.enable(bullet, false);
+        bullet.body.dynamic = true;
+        bullet.anchor.setTo(0.5, 0.5);
+        bullet.body.damping = 0; //part of velocity losed per second [0..1];
+        bullet.body.setCircle(7);
+        bullet.body.fixedRotation = false;
         bullet.body.collideWorldBounds = true;
-		bullet.kill();
-		bullet.body.onBeginContact.add(this.bulletHit, this, 0, bullet);
-		bullet.countHit = 0;//еще не ударялась.
-		this.bullets.push(bullet);
-	}
+        bullet.kill();
+        bullet.body.onBeginContact.add(this.bulletHit, this, 0, bullet);
+        bullet.countHit = 0;//еще не ударялась.
+        this.bullets.push(bullet);
+    }
 }
 
 Weapon.prototype.bulletHit = function(body, bodyB, shapeA, shapeB, equation, bullet) { // обработчик касания пули с объектом body
-	bullet.countHit += 1;
-	if(bullet.countHit > this.max_coll) {
-		bullet.kill();
-		return;
-	}
-	if (body) {//hit
-		if (body.isPlayer!=undefined) {
-			dbg = "hit player";
-			body.sprite.damage(5);
-		} else {
-			dbg = "body:" + body.sprite.key + " bodyB: " + bodyB.sprite;
-		}
-	} else { //bullet hit the wall
-		dbg = "bullet hit the wall";
-	}
+    bullet.countHit += 1;
+    if(bullet.countHit > this.max_coll) {
+        bullet.kill();
+        return;
+    }
+    if (body) {//hit
+        if (body.isPlayer!=undefined) {
+            dbg = "hit player";
+            body.sprite.damage(5);
+        } else {
+            dbg = "body:" + body.sprite.key + " bodyB: " + bodyB.sprite;
+        }
+    } else { //bullet hit the wall
+        dbg = "bullet hit the wall";
+    }
 
 }
 
@@ -75,82 +75,82 @@ Weapon.prototype.fire = function() {
     if(this.needs_reload) {
         return;
     }
-	let ms = Date.now();
-	//console.log(ms, this.prev_fire);
-	if(ms - this.prev_fire >= this.fire_rate) {
-		if(this.prev_fire != -1000) {
-			this.bullets_in_row++;
-		}
-		else {
-			this.bullets_in_row = 0;
-		}
+    let ms = Date.now();
+    //console.log(ms, this.prev_fire);
+    if(ms - this.prev_fire >= this.fire_rate) {
+        if(this.prev_fire != -1000) {
+            this.bullets_in_row++;
+        }
+        else {
+            this.bullets_in_row = 0;
+        }
 
-		this.prev_fire = ms;
+        this.prev_fire = ms;
 
-		let bullet = this.bullets[this.bullet_ind];
-		let angle_variance = 0;
-		if(this.bullets_in_row <= 6) {
-	 		angle_variance = 0.05*this.bullets_in_row;
-		}
-		else {
-			angle_variance = 0.05*6;
-		}
-		
-		this.player_sprite.animations.stop();
-		if (this.player_sprite.frame == 0) {
-			this.player_sprite.animations.play('fire0');
-		} else if (this.player_sprite.frame == 1) {
-			this.player_sprite.animations.play('fire1');
-		} else if (this.player_sprite.frame == 2) {
-			this.player_sprite.animations.play('fire2');
-		}
-		
-		bullet.countHit = 0;
-		bullet.body.rotation = this.player_sprite.body.rotation;
-		bullet.body.x = this.player_sprite.body.x + 93*Math.cos(bullet.body.rotation);
-		bullet.body.y = this.player_sprite.body.y + 93*Math.sin(bullet.body.rotation);
-		bullet.body.moveRight(this.bullet_speed*(Math.cos(bullet.body.rotation) + random(-angle_variance, angle_variance)));
-		bullet.body.moveDown(this.bullet_speed*(Math.sin(bullet.body.rotation) + random(-angle_variance, angle_variance)));
-		bullet.revive();
+        let bullet = this.bullets[this.bullet_ind];
+        let angle_variance = 0;
+        if(this.bullets_in_row <= 6) {
+            angle_variance = 0.05*this.bullets_in_row;
+        }
+        else {
+            angle_variance = 0.05*6;
+        }
+        
+        this.player_sprite.animations.stop();
+        if (this.player_sprite.frame == 0) {
+            this.player_sprite.animations.play('fire0');
+        } else if (this.player_sprite.frame == 1) {
+            this.player_sprite.animations.play('fire1');
+        } else if (this.player_sprite.frame == 2) {
+            this.player_sprite.animations.play('fire2');
+        }
+        
+        bullet.countHit = 0;
+        bullet.body.rotation = this.player_sprite.body.rotation;
+        bullet.body.x = this.player_sprite.body.x + 93*Math.cos(bullet.body.rotation);
+        bullet.body.y = this.player_sprite.body.y + 93*Math.sin(bullet.body.rotation);
+        bullet.body.moveRight(this.bullet_speed*(Math.cos(bullet.body.rotation) + random(-angle_variance, angle_variance)));
+        bullet.body.moveDown(this.bullet_speed*(Math.sin(bullet.body.rotation) + random(-angle_variance, angle_variance)));
+        bullet.revive();
 
-		this.bullet_ind++;
-		if(this.bullet_ind == this.num_bullets) {
-			this.bullet_ind = 0;
+        this.bullet_ind++;
+        if(this.bullet_ind == this.num_bullets) {
+            this.bullet_ind = 0;
             this.needs_reload = true;
             console.log('Yes!');
-		}
-	}
+        }
+    }
 };
 
 Weapon.prototype.null_fire = function() {
-	this.prev_fire = -1000;
+    this.prev_fire = -1000;
 };
 
 Weapon.prototype.update = function() {
-	for(let i = 0; i < this.num_bullets; i++) {
-		let bullet = this.bullets[i];
-		if(bullet.alive) {
-			bullet.body.rotation = Math.atan2(bullet.body.velocity.y, bullet.body.velocity.x);
-			//console.log(bullet.body.rotation);
-		}
-	}
+    for(let i = 0; i < this.num_bullets; i++) {
+        let bullet = this.bullets[i];
+        if(bullet.alive) {
+            bullet.body.rotation = Math.atan2(bullet.body.velocity.y, bullet.body.velocity.x);
+            //console.log(bullet.body.rotation);
+        }
+    }
 };
 
 function Player(sprite_name, weapon_settings, follow_camera) {
     this.player_sprite = game.add.sprite(0, 0, sprite_name);
-	game.physics.p2.enable(this.player_sprite, true);//true - debug
-	this.player_sprite.frame = 1;
-	this.player_sprite.animations.add('go', [1, 0, 1, 2], 7, true);
-	this.player_sprite.animations.add('fire0', [3, 0], 10, false);
-	this.player_sprite.animations.add('fire1', [4, 1], 10, false);
-	this.player_sprite.animations.add('fire2', [5, 2], 10, false);
-	this.player_sprite.enableBody = true;
-	this.player_sprite.body.collideWorldBounds = true;
-	this.player_sprite.anchor.setTo(0.45, 0.54);
-	this.player_sprite.body.setCircle(65);
-	this.player_sprite.body.isPlayer = 1;
-	this.player_sprite.setHealth(100);
-	this.player_sprite.events.onKilled = killedHook(this);
+    game.physics.p2.enable(this.player_sprite, true);//true - debug
+    this.player_sprite.frame = 1;
+    this.player_sprite.animations.add('go', [1, 0, 1, 2], 7, true);
+    this.player_sprite.animations.add('fire0', [3, 0], 10, false);
+    this.player_sprite.animations.add('fire1', [4, 1], 10, false);
+    this.player_sprite.animations.add('fire2', [5, 2], 10, false);
+    this.player_sprite.enableBody = true;
+    this.player_sprite.body.collideWorldBounds = true;
+    this.player_sprite.anchor.setTo(0.45, 0.54);
+    this.player_sprite.body.setCircle(65);
+    this.player_sprite.body.isPlayer = 1;
+    this.player_sprite.setHealth(100);
+    this.player_sprite.events.onKilled = killedHook(this);
 
     if(follow_camera) {
         game.camera.follow(this.player_sprite);
@@ -161,7 +161,7 @@ function Player(sprite_name, weapon_settings, follow_camera) {
 
 
     let {num_bullets, bullet_speed, fire_rate, max_coll, reloading_time} = weapon_settings;
-	this.weapon = new Weapon(this.player_sprite, num_bullets, bullet_speed, fire_rate, max_coll, reloading_time);
+    this.weapon = new Weapon(this.player_sprite, num_bullets, bullet_speed, fire_rate, max_coll, reloading_time);
 }
 
 Player.prototype.setXY = function(x, y) {
@@ -171,7 +171,7 @@ Player.prototype.setXY = function(x, y) {
 
 Player.prototype.update = function() {
     this.player_sprite.body.setZeroVelocity();
-	var vx = 0, vy = 0;
+    var vx = 0, vy = 0;
     if(this.weapon.reloading && (Date.now() - this.weapon.reloading_start >= this.weapon.reloading_time)) {
         this.weapon.reloading = false;
         this.weapon.needs_reload = false;
@@ -181,43 +181,43 @@ Player.prototype.update = function() {
         this.weapon.reloading_start = Date.now();
         console.log('Reloading!', this.weapon.reloading_start)
     }
-	if (cursors.left.isDown || game.input.keyboard.isDown(Phaser.Keyboard.A)) {
-		this.player_sprite.animations.play('go');
+    if (cursors.left.isDown || game.input.keyboard.isDown(Phaser.Keyboard.A)) {
+        this.player_sprite.animations.play('go');
         vx = -1;
     }
     else if (cursors.right.isDown || game.input.keyboard.isDown(Phaser.Keyboard.D)) {
-		this.player_sprite.animations.play('go');
+        this.player_sprite.animations.play('go');
         vx = 1;
     }
     if (cursors.up.isDown || game.input.keyboard.isDown(Phaser.Keyboard.W)) {
-		this.player_sprite.animations.play('go');
-		vy = -1;
+        this.player_sprite.animations.play('go');
+        vy = -1;
     }
     else if (cursors.down.isDown || game.input.keyboard.isDown(Phaser.Keyboard.S)) {
-		this.player_sprite.animations.play('go');
-		vy = 1;
+        this.player_sprite.animations.play('go');
+        vy = 1;
     }
     if (vx != 0 && vy != 0) {
-		this.player_sprite.body.moveDown(vy*vel/sq2);
-		this.player_sprite.body.moveRight(vx*vel/sq2);
-	} else {
-		this.player_sprite.body.moveDown(vy*vel);
-		this.player_sprite.body.moveRight(vx*vel);
-	}
+        this.player_sprite.body.moveDown(vy*vel/sq2);
+        this.player_sprite.body.moveRight(vx*vel/sq2);
+    } else {
+        this.player_sprite.body.moveDown(vy*vel);
+        this.player_sprite.body.moveRight(vx*vel);
+    }
     if((this.player_sprite.body.velocity.x == 0) && (this.player_sprite.body.velocity.y == 0)) {
-		this.player_sprite.animations.stop();
+        this.player_sprite.animations.stop();
         this.player_sprite.frame = 1;
-	}
+    }
 
     this.player_sprite.body.rotation = game.physics.arcade.angleToPointer(this.player_sprite);
 
-	this.weapon.update();
+    this.weapon.update();
     if(game.input.activePointer.leftButton.isDown) {
         this.weapon.fire();
     }
-	else {
-		this.weapon.null_fire();
-	}
+    else {
+        this.weapon.null_fire();
+    }
 }
 
 Player.prototype.render = function() {
@@ -241,14 +241,14 @@ Player.prototype.render = function() {
 
 
 function killedHook(player) {
-	
+    
 }
 
 function preload () {
-	game.load.spritesheet('player1_sprite', 'chelik_vertikalny.png', 174, 100);
-	game.load.image('box', 'wood.png');
-	game.load.image('bullet', 'sprites/bullet.png');
-	game.load.image('sc_background', 'back.jpg');
+    game.load.spritesheet('player1_sprite', 'chelik_vertikalny.png', 174, 100);
+    game.load.image('box', 'wood.png');
+    game.load.image('bullet', 'sprites/bullet.png');
+    game.load.image('sc_background', 'back.jpg');
 }
 
 var PI = 3.1414926535;
@@ -261,7 +261,7 @@ var b0, b1;
 var weapon, fire_button;
 var dbg = "Nothing happened";
 
-function generate_field(nb, nt, offset) {	//nb и nt - четные.
+function generate_field(nb, nt, offset) {   //nb и nt - четные.
     boxes = game.add.physicsGroup(Phaser.Physics.P2JS);
     for (let i = 0; i < nb; i++) {
         var x, y, good;
@@ -332,16 +332,16 @@ function generate_field(nb, nt, offset) {	//nb и nt - четные.
 }
 
 function create() {
-	game.time.advancedTiming = true;
-	game.input.mouse.capture = true;
-	game.physics.startSystem(Phaser.Physics.P2JS);
-	game.physics.p2.restitution = 0.8;
-	game.stage.backgroundColor = "#EEEEEE";
+    game.time.advancedTiming = true;
+    game.input.mouse.capture = true;
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.p2.restitution = 0.8;
+    game.stage.backgroundColor = "#EEEEEE";
     game.world.setBounds(0, 0, 1920, 1920);
     
     var background = game.add.tileSprite(0, 0, 1920, 1920, 'sc_background');
 
-	player1 = new Player('player1_sprite', {
+    player1 = new Player('player1_sprite', {
         num_bullets: 30,
         bullet_speed: 800,
         fire_rate: 60,
@@ -359,53 +359,53 @@ function create() {
 
     //player2.setXY(100, 200);
 
-	cursors = game.input.keyboard.createCursorKeys();
-	game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
-	game.input.keyboard.addKeyCapture([ Phaser.Keyboard.A ]);
-	game.input.keyboard.addKeyCapture([ Phaser.Keyboard.S ]);
-	game.input.keyboard.addKeyCapture([ Phaser.Keyboard.D ]);
-	game.input.keyboard.addKeyCapture([ Phaser.Keyboard.W ]);
+    cursors = game.input.keyboard.createCursorKeys();
+    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
+    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.A ]);
+    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.S ]);
+    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.D ]);
+    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.W ]);
     game.input.keyboard.addKeyCapture([ Phaser.Keyboard.R ]);
 
-	/*boxes = game.add.physicsGroup(Phaser.Physics.P2JS);
-	var b0 = boxes.create(400, 200, 'box');//случайные координаты где-то в середине.
-	b0.anchor.setTo(0.5, 0.5);
-	b0.body.kinematic = true;
-	b0.body.rotation = 0.8243;//случайное, вообще-то число
-	var b1 = boxes.create(500, 400, 'box');//случайные координаты где-то в середине.
-	b1.anchor.setTo(0.5, 0.5);
-	b1.body.kinematic = true;
-	b1.body.rotation = 1.8243;//случайное, вообще-то число*/
+    /*boxes = game.add.physicsGroup(Phaser.Physics.P2JS);
+    var b0 = boxes.create(400, 200, 'box');//случайные координаты где-то в середине.
+    b0.anchor.setTo(0.5, 0.5);
+    b0.body.kinematic = true;
+    b0.body.rotation = 0.8243;//случайное, вообще-то число
+    var b1 = boxes.create(500, 400, 'box');//случайные координаты где-то в середине.
+    b1.anchor.setTo(0.5, 0.5);
+    b1.body.kinematic = true;
+    b1.body.rotation = 1.8243;//случайное, вообще-то число*/
     generate_field(16, 2, 165);
 }
 
 function update() {
-	player1.update();
+    player1.update();
 }
 
 function render() {
     player1.render();
-	game.debug.text(dbg, 32, 32);
-	game.debug.text(game.time.fps, 2, 14, "#00ff00");
-	game.debug.body(player1.player_sprite);
+    game.debug.text(dbg, 32, 32);
+    game.debug.text(game.time.fps, 2, 14, "#00ff00");
+    game.debug.body(player1.player_sprite);
 }
 
 var __point = new Phaser.Point(0, 0);
 function intersects(x, y) {
-	__point.set(x, y);
-	if (game.physics.p2.hitTest(__point).length == 0) return false;
-	else return true;
+    __point.set(x, y);
+    if (game.physics.p2.hitTest(__point).length == 0) return false;
+    else return true;
 }
 
 function getFreePoints(n) {
-	var ans = [];
-	for (var i = 0; i < n; i++) {
-		var x, y;
-		do {
-			x = getRandomInt(0, game.world.width);
-			y = getRandomInt(0, game.world.height);
-		} while (intersects(x, y));
-		ans[i] = [x, y];
-	}
-	return ans;
+    var ans = [];
+    for (var i = 0; i < n; i++) {
+        var x, y;
+        do {
+            x = getRandomInt(0, game.world.width);
+            y = getRandomInt(0, game.world.height);
+        } while (intersects(x, y));
+        ans[i] = [x, y];
+    }
+    return ans;
 }
